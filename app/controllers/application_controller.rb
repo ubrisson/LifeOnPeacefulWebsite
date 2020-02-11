6ac745a64324ceec5209b2c7cd2f5b627a320d27
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-11 15:36:22 +0100

    Add welcoming message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def welcome
-    render text: 'Welcome Home'
+    render html: 'Welcome Home'
   end
 end
