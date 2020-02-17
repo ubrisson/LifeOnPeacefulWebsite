7fc8936e38ff2ee11b4e3c06008b2d6cca876dbd
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-17 15:08:12 +0100

    Added site navigation test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test 'navigation_links' do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path
+    assert_select 'a[href=?]', about_path
+  end
+end
