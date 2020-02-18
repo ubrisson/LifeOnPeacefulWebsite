fed22b3d8d96e9848c0a0b06ebd4eec21e4d3fc4
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-18 22:53:37 +0100

    Generate sessions & add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/resources', to: 'static_pages#resources'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
