a83a812776f47402e9960ee852367ddb508c6278
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-20 14:31:40 +0100

    Add validation rules and tests for contents

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,9 +1,21 @@
 class Content < ApplicationRecord
+  self.inheritance_column = nil
+
   acts_as_taggable
+
   default_scope -> { order(created_at: :desc) }
-  validates :title, presence: true, uniqueness: {case_sensitive: false}, length: { maximum: 120 }
 
-  validates :body, presence: true, uniqueness: {case_sensitive: false}
+  validates :title, presence: true, length: { maximum: 120 }
 
+  validates :body, presence: true
+
+  validates :type, presence: true, inclusion: { in: %w[post quote]}
+
+  validates :author, presence: true, if: :quote?
+  validates :source, presence: true, if: :quote?
+
+  def quote?
+    type == 'quote'
+  end
 
 end
--- test/models/content_test.rb
+++ test/models/content_test.rb
@@ -3,10 +3,16 @@ require 'test_helper'
 class ContentTest < ActiveSupport::TestCase
 
   def setup
-    # This code is not idiomatically correct.
-    @content = Content.new(body: "Lorem ipsum", title:"test")
+    @content = Content.new(body: "Lorem ipsum", title:"test", type:"post")
   end
-  test "should be valid" do
+  test "post should be valid" do
+    assert @content.valid?
+  end
+
+  test "quote should be valid" do
+    @content.type = "quote"
+    @content.source = "example.org"
+    @content.author = "ego"
     assert @content.valid?
   end
 
@@ -16,16 +22,28 @@ class ContentTest < ActiveSupport::TestCase
   end
 
   test "content should be present" do
-    @content.content = " "
+    @content.body = " "
     assert_not @content.valid?
   end
 
   test "order should be most recent first" do
-    assert_equal contents(:most_recent), contents.first
+    assert_equal contents(:most_recent), Content.first
   end
 
-  test "type should be post or quote" do
+  test "type should be present" do
     @content.type = nil
+    assert_not @content.valid?
+  end
+
+  test "quotes should have author" do
+    @content.type = "quote"
+    @content.author = nil
+    assert_not @content.valid?
+  end
+
+  test "quotes should have source" do
+    @content.type = "quote"
+    @content.source = nil
     assert_not @content.valid?
   end
 
