e11354a9e2db27579384494b03bb69f08fcea8cc
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-20 18:57:04 +0100

    Add initial random data for contents

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |_|
+  title = Faker::Book.title
+  body = Faker::Movie.quote
+  Content.create(title: title, body: body, type: 'post')
+end
+
+50.times do |_|
+  title = Faker::Book.genre
+  body = Faker::Movie.quote
+  author = Faker::Book.author
+  source = Faker::Book.title
+  Content.create(title: title, body: body, type: 'quote', author: author, source: source)
+end
