179a4ccf47ccc9ae26b103417a21e5311c65c852
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-24 14:35:40 +0100

    Add show & edit actions and routes

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,5 +1,9 @@
 class ResourcesController < ApplicationController
 
+  def show
+    @resource = Resource.find(params[:id])
+  end
+
   def index
     @resources = Resource.paginate(page: params[:page])
   end
@@ -7,20 +11,37 @@ class ResourcesController < ApplicationController
   def create
     @resource = Resource.new(resource_params)
     if @resource.save
-      flash[:success] = "Resource created."
+      flash[:success] = 'Resource created.'
     else
-      flash[:danger] = "Unable to create resource."
+      flash[:danger] = 'Unable to create resource.'
     end
     redirect_to resources_path
   end
 
+  def edit
+    store_location
+    @resource = Resource.find(params[:id])
+  end
+
   def update
+    @resource = Resource.find(params[:id])
+    if @resource.update(resource_params)
+      flash[:success] = 'Resource edited'
+      redirect_back_or @resource
+    else
+      flash[:danger] = 'Failed edition.'
+      render 'resources/edit'
+    end
   end
 
   def destroy
+    Resource.delete(params[:id])
+    flash[:success] = 'Resource deleted'
+    redirect_to request.referrer || root_url
   end
 
   private
+
   def resource_params
     params.require(:resource).permit(:title, :author, :description, :link,
                                      :publication, :tag_list)
