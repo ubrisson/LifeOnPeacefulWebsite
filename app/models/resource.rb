fa6d67aeefcdbcd2efb68bfa991513131fd2aea2
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-25 11:27:04 +0100

    Improve tag text_field

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,11 +1,19 @@
 class Resource < ApplicationRecord
   acts_as_taggable
   default_scope -> { order(created_at: :desc) }
-  validates :title, presence: true, length: { maximum: 144 }
-  validates :author, presence: true, length: { maximum: 120 }
+  validates :title, presence: true, length: {maximum: 144}
+  validates :author, presence: true, length: {maximum: 120}
   validates :description, presence: true, allow_blank: true
-  validates :link, presence: true, allow_blank: true, length: { maximum: 120 }
+  validates :link, presence: true, allow_blank: true, length: {maximum: 120}
 
   # TODO Validate and test publication date ?
   # TODO Validate and test link url
+
+  def form_tags
+    str = ''
+    tag_list.each do |tag|
+      str += tag + ', '
+    end
+    str
+  end
 end
