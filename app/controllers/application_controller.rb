c318d079537aecb1c395224619f7aaf9a7268f73
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-26 09:13:08 +0100

    Added admin before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+
+  # Before filter: Confirms an admin user else redirects
+  def admin_user
+    redirect_to(root_url) unless admin?
+  end
 end
