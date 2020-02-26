5b689aca9006319b4121ec2b3c220b915e2c3626
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-26 09:11:25 +0100

    Added scope and "public" validation

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,14 +1,16 @@
 class Resource < ApplicationRecord
   acts_as_taggable
+  scope :only_public, -> { where(public: true) }
   scoped_search on: %i[title author description]
   default_scope -> { order(created_at: :desc) }
-  validates :title, presence: true, length: {maximum: 144}
-  validates :author, presence: true, length: {maximum: 120}
+  validates :title, presence: true, length: { maximum: 144 }
+  validates :author, presence: true, length: { maximum: 120 }
   validates :description, presence: true, allow_blank: true
-  validates :link, presence: true, allow_blank: true, length: {maximum: 120}
+  validates :link, presence: true, allow_blank: true, length: { maximum: 120 }
+  validates :public, inclusion: { in: [true, false] }
 
-  # TODO Validate and test publication date ?
-  # TODO Validate and test link url
+  # TODO. Validate and test publication date ?
+  # TODO. Validate and test link url
 
   def form_tags
     str = ''
