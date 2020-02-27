62315af5974c04e75c933abfcd45cfd4cd6ff74e
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-27 13:12:48 +0100

    Add logout testing

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -12,18 +12,27 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test 'login with valid information' do
-    # get the login page
+  test 'login with valid information followed by logout' do
     get '/login'
     assert_equal 200, status
     @user = users(:admin)
     post '/login', params: { session: { name: @user.name,
                                         password: 'password' } }
+    assert is_logged_in?
     assert_redirected_to root_url
     follow_redirect!
     assert_equal 200, status
     assert_select 'h3', text: "Welcome #{@user.name}"
-    assert is_logged_in?
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
   end
 
 end
