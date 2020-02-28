adbf3702a48cd86dfa79c9ab8475f2511a7b5151
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-28 13:28:21 +0100

    Paste and adapt controller tests from resources

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -63,4 +63,161 @@ class QuotesControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to quotes_url
   end
+
+  test 'should import quotes' do
+    log_in_as(@admin)
+    assert_difference 'Quote.count', 3 do
+      post quotes_import_path,
+           params: { json: @quotes_to_import }
+    end
+    assert_not flash.empty?
+    assert_redirected_to quotes_path
+  end
+
+  test 'should not create with invalid information' do
+    log_in_as(@admin)
+    assert is_logged_in?
+    assert_no_difference 'Quote.count' do
+      post quotes_path, params: { quote: { author: @quote.author,
+                                           commentary: @quote.commentary,
+                                           source: @quote.source,
+                                           title: @quote.title } }
+      assert_redirected_to quotes_path
+      assert_not flash.empty?
+      assert_select 'h3', text: @quote.title, count: 0
+    end
+  end
+
+  test 'should not update with invalid information' do
+    log_in_as(@admin)
+    assert is_logged_in?
+    patch quote_url(@quote), params: { quote: { body: '' } }
+    assert_redirected_to edit_quote_path(@quote)
+    assert_not flash.empty?
+  end
+  # endregion
+
+  # region search and tags
+  test 'should get tagged quote' do
+    @quote.tag_list = 'example_tag'
+    @quote.save
+    get quotes_path, params: { tag: 'example_tag' }
+    assert_select 'a', text: '#example_tag'
+    assert_select 'article', count: 1
+  end
+
+  test 'should get searchable quote' do
+    get quotes_path, params: { q: 'orange' }
+    assert_select 'article', count: 1
+  end
+
+  test 'should get tagged and searchable quote' do
+    not_orange = quotes(:two)
+    not_orange.tag_list = 'orange'
+    not_orange.save
+    get quotes_path, params: { q: 'orange' }
+    assert_select 'article', count: 2
+  end
+  # endregion
+
+  # region before filter redirection tests
+  test 'should redirect new when not logged in' do
+    get new_quote_url
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect new when logged in as a non-admin' do
+    log_in_as(@not_admin)
+    get new_quote_url
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect create when not logged in' do
+    post quotes_path, params: { quote: { author: @quote.author,
+                                         body: @quote.body,
+                                         commentary: @quote.commentary,
+                                         public: @quote.public,
+                                         source: @quote.source,
+                                         title: @quote.title } }
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect create when logged in as a non-admin' do
+    log_in_as(@not_admin)
+    post quotes_path, params: { quote: { author: @quote.author,
+                                         body: @quote.body,
+                                         commentary: @quote.commentary,
+                                         public: @quote.public,
+                                         source: @quote.source,
+                                         title: @quote.title } }
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect edit when not logged in' do
+    get edit_quote_path(@quote)
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect edit when logged in as a non-admin' do
+    log_in_as(@not_admin)
+    get edit_quote_path(@quote)
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch quote_path(@quote), params: { quote: { title: @quote.title } }
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect update when logged in as a non-admin' do
+    log_in_as(@not_admin)
+    patch quote_path(@quote), params: { quote: { title: @quote.title } }
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'Quote.count' do
+      delete quote_path(@quote)
+    end
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect destroy when logged in as a non-admin' do
+    log_in_as(@not_admin)
+    assert_no_difference 'Quote.count' do
+      delete quote_path(@quote)
+    end
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect import when not logged in' do
+    post quotes_import_path,
+         params: { json: @quotes_to_import }
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect import when logged in as a non-admin' do
+    log_in_as(@not_admin)
+    post quotes_import_path,
+         params: { json: @quotes_to_import }
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+  # endregion
+
+  test 'should export quotes' do
+    get quotes_export_path
+    assert_response :success
+  end
 end
