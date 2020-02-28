73b379f3dd057bdedba18561afcdf3bd3f20e613
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-28 09:28:40 +0100

    Add tags, search, and quote validation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,19 @@
 class Quote < ApplicationRecord
+  acts_as_taggable
+  scope :only_public, -> { where(public: true) }
+  scoped_search on: %i[title author description]
+  default_scope -> { order(created_at: :desc) }
+  validates :title, presence: true, length: { maximum: 144 }
+  validates :author, presence: true, length: { maximum: 120 }
+  validates :source, presence: true, length: { maximum: 120 }
+  validates :commentary, presence: true, allow_blank: true
+  validates :public, inclusion: { in: [true, false] }
+
+  def form_tags
+    str = ''
+    tag_list.each do |tag|
+      str += tag + ', '
+    end
+    str
+  end
 end
