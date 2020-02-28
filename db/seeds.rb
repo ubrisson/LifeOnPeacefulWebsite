765a4c468071445f55cc3da6ea5ac532f157eb7d
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-02-28 10:28:08 +0100

    Add quotes seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-50.times do |_|
-  title = Faker::Book.title
-  body = Faker::Movie.quote
-  Content.create(title: title, body: body, type: 'post')
-end
+# 50.times do |_|
+#   title = Faker::Book.title
+#   body = Faker::Movie.quote
+#   Content.create(title: title, body: body, type: 'post')
+# end
+#
+# 50.times do |_|
+#   title = Faker::Book.genre
+#   body = Faker::Movie.quote
+#   author = Faker::Book.author
+#   source = Faker::Book.title
+#   Content.create(title: title, body: body, type: 'quote', author: author, source: source)
+# end
+#
+# 50.times do |_|
+#   title = Faker::Book.title
+#   body = Faker::Book.genre
+#   author = Faker::Book.author
+#   source = Faker::Address.full_address
+#   publication = Faker::Date.backward
+#   Resource.create(title: title,
+#                   description: body,
+#                   author: author,
+#                   link: source,
+#                   publication: publication,
+#                   tag_list: 'resource, untagged')
+# end
 
 50.times do |_|
   title = Faker::Book.genre
   body = Faker::Movie.quote
   author = Faker::Book.author
   source = Faker::Book.title
-  Content.create(title: title, body: body, type: 'quote', author: author, source: source)
-end
-
-50.times do |_|
-  title = Faker::Book.title
-  body = Faker::Book.genre
-  author = Faker::Book.author
-  source = Faker::Address.full_address
-  publication = Faker::Date.backward
-  Resource.create(title: title,
-                  description: body,
+  commentary = Faker::ChuckNorris.fact
+  Quote.create(title: title,
+                  body: body,
                   author: author,
-                  link: source,
-                  publication: publication,
-                  tag_list: 'resource, untagged')
+                  source: source,
+                  tag_list: "quote, #{title}",
+                  commentary: commentary,
+                  public: false)
 end
