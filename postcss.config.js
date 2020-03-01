dfe294ce418baa6675d80435efb6ab40875bc54d
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-03-01 11:45:35 +0100

    Correcting css pipeline and tailwind integration

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 let environment = {
     plugins: [
-        require('postcss-import'),
         require('tailwindcss'),
+        require('autoprefixer'),
+        require('postcss-import'),
         require('postcss-flexbugs-fixes'),
         require('postcss-preset-env')({
             autoprefixer: {
