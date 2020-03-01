418d9d5f4fb7e301f8b7f18420d6e6bb8d486095
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-03-01 20:34:20 +0100

    Allow blank body and search on rich_text

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,12 @@ class Post < ApplicationRecord
   has_rich_text :body
   acts_as_taggable
   scope :only_public, -> { where(public: true) }
-  scoped_search on: %i[title body]
+  scoped_search on: %i[title]
+  scoped_search relation: :rich_text_body, on: :body
   default_scope -> { order(created_at: :desc) }
 
   validates :title, presence: true, length: { maximum: 144 }
-  validates :body, presence: true
+  validates :body, presence: true, allow_blank: true
   validates :public, inclusion: { in: [true, false] }
 
 
