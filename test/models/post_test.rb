e89ce7b357f17a59f7ef0e3e9a868b6636714f62
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-03-01 20:35:36 +0100

    Adapt fixtures and add form_tags test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'form_tags should return tags between comas' do
+    post = posts(:one)
+    post.tag_list = "full, resource, example, tag"
+    post.save
+    assert_equal 'full, resource, example, tag, ', posts(:one).form_tags
+  end
 end
