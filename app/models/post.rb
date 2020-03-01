2b34c09fe47c15d589c138710e24dc54402a5ca2
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-03-01 18:35:35 +0100

    Add model post tags and validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,20 @@
 class Post < ApplicationRecord
   has_rich_text :body
+  acts_as_taggable
   scope :only_public, -> { where(public: true) }
   scoped_search on: %i[title body]
   default_scope -> { order(created_at: :desc) }
+
+  validates :title, presence: true, length: { maximum: 144 }
+  validates :body, presence: true
+  validates :public, inclusion: { in: [true, false] }
+
+
+  def form_tags
+    str = ''
+    tag_list.each do |tag|
+      str += tag + ', '
+    end
+    str
+  end
 end
