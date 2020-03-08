dfbb7fa745300713d7da98a08ae424550d752db4
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-03-08 16:00:27 +0100

    Refactor current_user (cleaner)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,11 +7,7 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any).
   def current_user
-    if session[:user_id].nil?
-      @current_user = nil
-    else
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   # Returns true if the user is logged in, false otherwise.
