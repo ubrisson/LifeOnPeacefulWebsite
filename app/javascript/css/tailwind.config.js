f897dba9b62019178725522fe4e79bd2d9ad5473
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-03-10 15:59:08 +0100

    Improve breakpoints

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -5,9 +5,16 @@ module.exports = {
         extend: {
             width: {
                 '1/10': '10%',
+                '1/8': '12,5%',
             },
             screens: {
                 '2xl': '1440px',
+                '3xl': '1700px',
+                'nymous':'2500px',
+            },
+            minWidth: {
+                '1/10': '10%',
+                '1/8': '12.5%',
             }
         }
     }
