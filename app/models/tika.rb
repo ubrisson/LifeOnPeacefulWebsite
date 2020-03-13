9e83cf7ddfcdb38394b33bf622c984508864332a
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-03-13 16:40:01 +0100

    Add parent autocompletion and ordering

--- app/models/tika.rb
+++ app/models/tika.rb
@@ -1,3 +1,10 @@
 class Tika < ApplicationRecord
+  before_save do
+    title.downcase!
+  end
   has_ancestry
+  default_scope -> { order(title: :asc) }
+  validates :title, presence: true,
+                    length: { maximum: 144 },
+                    uniqueness: { case_sensitive: false }
 end
