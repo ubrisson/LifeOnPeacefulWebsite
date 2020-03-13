bd221d18b6a5124f7f48082d77a7a9eab611ac4f
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-03-13 10:54:01 +0100

    Add tikas tests and title => h3 (masterlist is h2)

--- test/controllers/tikas_controller_test.rb
+++ test/controllers/tikas_controller_test.rb
@@ -1,24 +1,57 @@
 require 'test_helper'
 
 class TikasControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tikas_index_url
-    assert_response :success
+
+  setup do
+    @admin = users(:admin)
+    @tika = tikas(:one)
   end
 
-  test "should get create" do
-    get tikas_create_url
-    assert_response :success
+  test 'should redirect if not admin' do
+    get tikas_url
+    assert_not flash.empty?
+    assert_redirected_to root_url
   end
 
-  test "should get update" do
-    get tikas_update_url
+  test 'should get index if admin' do
+    log_in_as(@admin)
+    assert is_logged_in?
+    get tikas_url
     assert_response :success
+    assert_select 'h2', text: 'Masterlist'
   end
 
-  test "should get delete" do
-    get tikas_delete_url
-    assert_response :success
+  test 'should create tika' do
+    log_in_as(@admin)
+    assert is_logged_in?
+    assert_difference 'Tika.count', 1 do
+      post tikas_url, params: {tika: {title: 'test'}}
+    end
+    assert_not flash.empty?
+    assert_redirected_to tikas_path
   end
+
+  test 'should delete tika' do
+    log_in_as(@admin)
+    assert is_logged_in?
+    assert_difference 'Tika.count', -1 do
+      delete tika_path(@tika)
+    end
+    assert_not flash.empty?
+    assert_redirected_to tikas_path
+  end
+
+  test 'should update tika' do
+    log_in_as(@admin)
+    assert is_logged_in?
+    patch tika_path(@tika), params: { tika: { title: 'test' } }
+    assert_not flash.empty?
+    assert_redirected_to tikas_path
+    follow_redirect!
+    @tika.reload
+    assert_equal @tika.title, 'test'
+    assert_select 'h3', text: 'test'
+  end
+
 
 end
