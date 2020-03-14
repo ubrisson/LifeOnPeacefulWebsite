1fe1acd10b7b61aec4e1c44d78dc642891fac9e6
ubrisson <50323620+ubrisson@users.noreply.github.com>
2020-03-14 15:02:01 +0100

    Add themes

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -15,6 +15,25 @@ module.exports = {
             minWidth: {
                 '1/10': '10%',
                 '1/8': '12.5%',
+            },
+            textColor: {
+                primary: "var(--color-text-primary)",
+                secondary  : "var(--color-text-secondary)",
+                default: "var(--color-text-default)",
+                "default-soft": "var(--color-text-default-soft)",
+                inverse: "var(--color-text-inverse)",
+                "inverse-soft": "var(--color-text-inverse-soft)"
+            },
+            backgroundColor: {
+                primary: "var(--color-bg-primary)",
+                secondary: "var(--color-bg-secondary)",
+                default: "var(--color-bg-default)",
+                inverse: "var(--color-bg-inverse)",
+                tertiary: "var(--color-bg-tertiary)"
+            },
+            borderColor: {
+                primary: "var(--color-border-primary)",
+                secondary: "var(--color-border-secondary)",
             }
         }
     }
